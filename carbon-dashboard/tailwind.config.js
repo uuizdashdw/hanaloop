a531f6441d6efc6c0b3b9e3d2bed0a55311a2f88
cwKang-mcb <chanwoong.kang@mcbglobal.pro>
2025-09-23 15:15:42 +0900

    refactor : 테일윈드 세팅

---
+++ carbon-dashboard/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  source: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
